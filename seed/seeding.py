6e489acf588f10476f0f2fcdcc78eb5e22820a75
David Flanik <Daveflanik@gmail.com>
2025-05-17 22:43:35 +0900

    added session context

--- seed/seeding.py
+++ seed/seeding.py
@@ -2,9 +2,9 @@ from modles.workspace_service import create_workspace, WorkspaceCreateRequest
 
 def seed_workspace():
     data = WorkspaceCreateRequest(
-        workspace_id="abc123",
-        workspace_name="Seeded Workspace",
-        token="xoxb-1234"
+        workspace_id="abc1234",
+        workspace_name="Seeded Workspace2",
+        token="xoxb-12345"
     )
     create_workspace(data)
     print("Seeded workspace.")
